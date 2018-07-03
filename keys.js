bb46ca387f08b8b0445334242b32bdd8ed3a2ef1
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-07-02 19:45:30 -0500

    created the liri app

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+    consumer_key: 'yT3KPpBExbAqFOvek8j0K4qMa',
+    consumer_secret: 'zuLwI6p840YiDIZMq6D6iHKI4DjugUB7gTbD6sMfzgvvBBbOX5',
+    access_token_key: '1945133678-bLaMTIDFga7NRttvj2H3kWHfe8Pko3ezzB6ZTcq',
+    access_token_secret: 'XXa4UJ2Yi8JJKMvtufabxvLzDuxwRePWRi2dFrocUOAYM',
+  }
+  
+  exports.spotifyKeys = {
+      id: '7d094eed73d4492cac1e82fcb992d062',
+      secret: '75e15db3aef2404a9a74d5cdcea1ba8f',
+  }
